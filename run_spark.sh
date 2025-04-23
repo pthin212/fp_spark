ae68e805d23f35b452ba007ebd7cf0842a2fba3d
pthin212 <thinhduong2123@gmail.com>
2025-04-23 09:58:45 +0700

    Corrected logic

--- run_spark.sh
+++ run_spark.sh
@@ -4,14 +4,6 @@
 docker container stop fp-spark || true
 docker container rm fp-spark || true
 
-# Create a zip file of the project for distribution to workers
-rm -f 99-project/project.zip
-rm -f 99-project/pyspark_venv.tar.gz
-rm -rf pyspark_venv
-cd 99-project
-zip -r project.zip main.py udfs/ tables/ util/
-cd ..
-
 # Run the Spark container
 docker run -ti --name fp-spark \
   --network=streaming-network \
@@ -21,13 +13,14 @@ docker run -ti --name fp-spark \
   -v spark_lib:/opt/bitnami/spark/.ivy2 \
   -e PYSPARK_DRIVER_PYTHON='python' \
   -e PYSPARK_PYTHON='./environment/bin/python' \
-  unigap/spark:3.5 bash -c "cd /spark/99-project &&
+  unigap/spark:3.5 bash -c "
     python -m venv pyspark_venv &&
     source pyspark_venv/bin/activate &&
     pip install -r /spark/requirements.txt &&
     venv-pack -o pyspark_venv.tar.gz &&
+    tar -czvf project.tar.gz -C /spark/99-project main.py udfs/ tables/ util/
     spark-submit \
       --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.postgresql:postgresql:42.7.3 \
-      --py-files project.zip \
+      --py-files project.tar.gz \
       --archives pyspark_venv.tar.gz#environment \
-      main.py"
+      /spark/99-project/main.py"
